608c27164690d58fef2473b6228c79bd42463976
Karthikeyan25052001 <karthikeyan@highwirepress.com>
2024-08-29 15:50:19 +0530

    handled custom delimeter in string calculation add operation

--- assessment/src/main/java/com/karthik/assessment/StringCalculator.java
+++ assessment/src/main/java/com/karthik/assessment/StringCalculator.java
@@ -1,16 +1,23 @@
 package com.karthik.assessment;
 
 public class StringCalculator {
-
 	public int add(String numbers) {
+		if (numbers.isEmpty()) {
+			return 0;
+		}
+		String delimiter = ",";
+		if (numbers.startsWith("//")) {
+			int delimiterEndIndex = numbers.indexOf("\n");
+			delimiter = numbers.substring(2, delimiterEndIndex);
+			numbers = numbers.substring(delimiterEndIndex + 1);
+		}
 		int sum = 0;
-		if (!numbers.isEmpty()) {
-			String[] numsArr = numbers.split(",");
-			for (String num : numsArr) {
+		String[] numsArr = numbers.split("[,\n" + delimiter + "]");
+		for (String num : numsArr) {
+			if (!num.isEmpty()) {
 				sum += Integer.parseInt(num);
 			}
 		}
 		return sum;
 	}
-	
 }
--- assessment/src/test/java/com/karthik/assessment/StringCalculatorTest.java
+++ assessment/src/test/java/com/karthik/assessment/StringCalculatorTest.java
@@ -23,4 +23,23 @@ public class StringCalculatorTest {
 		StringCalculator strCalci = new StringCalculator();
 		assertEquals(3, strCalci.add("1,2"));
 	}
+	
+	@Test
+	public void testMultipleNumbers() {
+		StringCalculator strCalci = new StringCalculator();
+		assertEquals(5, strCalci.add("1,1,1,1,1"));
+		assertEquals(10, strCalci.add("1,2,3,4"));
+	}
+	
+	@Test
+	public void testNewLineDelimiter() {
+		StringCalculator strCalci = new StringCalculator();
+		assertEquals(6, strCalci.add("1\n2,3"));
+	}
+	
+	@Test
+	public void testCustomerDelimeter() {
+		StringCalculator strCalci = new StringCalculator();
+		assertEquals(3, strCalci.add("//;\n1;2"));
+	}
 }
