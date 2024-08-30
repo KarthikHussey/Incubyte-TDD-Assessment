6a66b60a873f2c286c770b7d555bae6b4055710b
Karthikeyan25052001 <karthikeyan@highwirepress.com>
2024-08-31 01:54:52 +0530

    Refactored String Calculator

--- assessment/src/main/java/com/karthik/assessment/StringCalculator.java
+++ assessment/src/main/java/com/karthik/assessment/StringCalculator.java
@@ -1,49 +1,72 @@
 package com.karthik.assessment;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StringCalculator {
+	
+	private static final String DEFAULT_DELIMITER = ",";
+    private static final String NEW_LINE = "\n";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String SPLIT_SEQUENCE = "\\]\\[";
+    private static final String OR = "|";
+    private static final int MAX_NUMBER = 1000;
 	
 	private int calledCount = 0;
 	
 	public int add(String numbers) {
 		calledCount++;
-		if (numbers.isEmpty()) {
+		if (numbers == null || numbers.isEmpty()) {
 			return 0;
 		}
-		String delimiter = ",";
-		if (numbers.startsWith("//")) {
-			int delimiterEndIndex = numbers.indexOf("\n");
-			String delimiterPart = numbers.substring(2, delimiterEndIndex);
-			numbers = numbers.substring(delimiterEndIndex + 1);
-			if (delimiterPart.startsWith("[")) {
-				delimiterPart = delimiterPart.substring(1, delimiterPart.length() - 1);
-				String[] delimiters = delimiterPart.split("\\]\\[");
-				delimiter = String.join("|", delimiters);
-			} else {
-				delimiter = delimiterPart;
-			}
-		}
-		int sum = 0;
-		StringBuilder negatives = new StringBuilder();
-		String[] numsArr = numbers.split("[,\n" + delimiter + "]");
-		for (String num : numsArr) {
-			if (num.isEmpty()) {
-				continue;
-			}
-			int numToAdd = Integer.parseInt(num);
-			if (numToAdd < 0) {
-				negatives.append(numToAdd);
-				negatives.append(",");
-			} else if (numToAdd <= 1000){
-				sum += numToAdd;
-			}
-		}
-		if (negatives.length() > 0) {
-			negatives.deleteCharAt(negatives.length() - 1);
-			throw new RuntimeException("negatives not allowed: " + negatives.toString());
-		}
-		return sum;
+		String delimiter = buildDelimiterRegex(extractDelimiter(numbers));
+		String nums = extractNumbers(numbers);
+		List<Integer> parsedNumbers = parseNumbers(nums, delimiter);
+		validate(parsedNumbers);
+		return calculateSum(parsedNumbers);
 	}
 
+	private String extractDelimiter(String numbers) {
+		if (!numbers.startsWith(CUSTOM_DELIMITER_PREFIX)) return DEFAULT_DELIMITER;
+		int delimiterEndIndex = numbers.indexOf(NEW_LINE);
+		String delimiter = numbers.substring(CUSTOM_DELIMITER_PREFIX.length(), delimiterEndIndex);
+		return convertToRegex(delimiter);
+	}
+	
+	private String convertToRegex(String delimiter) {
+		if (!delimiter.startsWith("[")) return delimiter;
+		String[] delimiters = delimiter.substring(1, delimiter.length() - 1).split(SPLIT_SEQUENCE);
+		return String.join(OR, delimiters);
+	}
+	
+	private String buildDelimiterRegex(String delimiter) {
+		StringBuilder regex = new StringBuilder();
+		regex.append("[").append(DEFAULT_DELIMITER).append(NEW_LINE).append(delimiter).append("]");
+		return regex.toString();
+	}
+	
+	private String extractNumbers(String numbers) {
+		if (!numbers.startsWith(CUSTOM_DELIMITER_PREFIX)) return numbers;
+		int delimiterEndIndex = numbers.indexOf(NEW_LINE);
+		return numbers.substring(delimiterEndIndex + 1);
+	}
+	
+	private List<Integer> parseNumbers(String numbers, String delimiter) {
+		return Arrays.stream(numbers.split(delimiter)).filter(num -> !num.isEmpty()).map(Integer :: parseInt).toList();
+	}
+	
+	private void validate(List<Integer> numbers) {
+		String negatives = numbers.stream().filter(number -> number < 0).map(String::valueOf).collect(Collectors.joining(","));
+		if (negatives.length() > 0) {
+			throw new RuntimeException("negatives not allowed: " + negatives);
+		}
+	}
+	
+	private int calculateSum(List<Integer> numbers) {
+		return numbers.stream().filter(number -> number <= MAX_NUMBER).mapToInt(Integer :: intValue).sum();
+	}
+	
 	public int getCalledCount() {
 		return calledCount;
 	}
